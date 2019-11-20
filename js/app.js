5d0322096ae37f5f5ab79b08b050365033a31c92
Tinh Phan <minhtinh885@gmail.com>
2019-11-20 13:04:59 +0700

    update Facebook quote

--- js/app.js
+++ js/app.js
@@ -1,20 +1,22 @@
-const siema = new Siema({
+const siema = document.querySelector('.siema') && new Siema({
     selector: '.siema',
     duration: 500,
     easing: 'ease-in-out',
     perPage: 1,
     loop: true,
 });
+if (siema !== null) {
+    const prev = document.querySelector('.slide-prev');
+    const next = document.querySelector('.slide-next');
+    if (prev !== null) {
+        prev.addEventListener('click', () => {
+            siema.prev();
+        });
+    }
+    if (next !== null) {
+        next.addEventListener('click', () => {
+            siema.next();
+        });
+    }
+}
 
-const prev = document.querySelector('.slide-prev');
-const next = document.querySelector('.slide-next');
-if (prev !== null) {
-    prev.addEventListener('click', () => {
-        siema.prev();
-    });
-}
-if (next !== null) {
-    next.addEventListener('click', () => {
-        siema.next();
-    });
-}
